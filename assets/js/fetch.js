6031d477c238989e412e4587a040ae230196ae17
Camilla Souto <104527029+Camis-souto@users.noreply.github.com>
2022-10-05 16:36:23 -0300

    Deploy

---
+++ assets/js/fetch.js
@@ -0,0 +1,35 @@
+const cep = document.querySelector('#cep');
+
+
+const showData = (result)=>{
+    for(const campo in result){
+        if(document.querySelector("#"+campo)){
+    document.querySelector("#" + campo).value = result[campo];
+        }
+    }
+}
+
+cep.addEventListener('blur', async (e)=>{
+    let search = cep.value.replace("-", "");
+    if(search.length < 8 || Number(search.value) === NaN){
+        alert("CEP inválido!");
+        cep.focus();
+        return;
+    }
+ const options = {
+    method: "GET",
+    mode: "cors",
+    cache: "default",
+ };
+ //axios - biblioteca javascript que usa fetch e XHR.
+ try{
+ const resultado = await fetch(
+    `https://viacep.com.br/ws/${search}/json/`,
+    options
+ );
+ const json = await resultado.json();
+ showData(json);
+ } catch (error) {
+    console.log("temos um problema", error.message);
+ }
+ });
